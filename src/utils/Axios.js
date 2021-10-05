81d3e5b836d660f1b42ef8ea7936bd9a63585001
VIGNESH-2406 <vigneshpaulraj4@gmail.com>
2021-10-05 17:08:38 +0530

    added

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = {
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: "https://money-manager-backend2.herokuapp.com",
   timeout: 30000,
 };
 
